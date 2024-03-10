3e3d0ae4f178dc7e26a464415cce185140a60b19
AndroPlus-org <hosigumayuugi@gmail.com>
2024-03-10 15:33:15 +0900

    Fxxk Xiaomi HyperOS

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -13,6 +13,33 @@ APILEVEL=$(getprop ro.build.version.sdk)
 mkdir -p $MODDIR/system/etc $MODDIR/system/system_ext/etc $MODDIR/system/product/etc
 cp /system/etc/fonts.xml $MODDIR/system/etc
 
+#Add workaround for Xiaomi HyperOS
+MIVERSION=$(getprop ro.miui.ui.version.code)
+if [ $MIVERSION -ge 816 ]
+then
+	if [ ! -f /data/system/theme/fonts/Roboto-Regular.ttf ]; then
+		mkdir /data/system/theme/fonts
+		cp $MODDIR/system/fonts/Mejiro-Regular.ttf /data/system/theme/fonts/Roboto-Regular.ttf
+		cp $MODDIR/system/fonts/Mejiro-Bold.ttf /data/system/theme/fonts/Roboto-Bold.ttf
+		ln -s /data/system/theme/fonts/Roboto-Bold.ttf /data/system/theme/fonts/Miui-Bold.ttf
+		ln -s /data/system/theme/fonts/Roboto-Regular.ttf /data/system/theme/fonts/Miui-Regular.ttf
+		ln -s /data/system/theme/fonts/Roboto-Bold.ttf /data/system/theme/fonts/MiuiEx-Bold.ttf
+		ln -s /data/system/theme/fonts/Roboto-Regular.ttf /data/system/theme/fonts/MiuiEx-Light.ttf
+		ln -s /data/system/theme/fonts/Roboto-Regular.ttf /data/system/theme/fonts/MiuiEx-Regular.ttf
+		ln -s /data/system/theme/fonts/Roboto-Bold.ttf /data/system/theme/fonts/Roboto-Black.ttf
+		ln -s /data/system/theme/fonts/Roboto-Bold.ttf /data/system/theme/fonts/Roboto-BlackItalic.ttf
+		ln -s /data/system/theme/fonts/Roboto-Bold.ttf /data/system/theme/fonts/Roboto-BoldItalic.ttf
+		ln -s /data/system/theme/fonts/Roboto-Regular.ttf /data/system/theme/fonts/Roboto-Italic.ttf
+		ln -s /data/system/theme/fonts/Roboto-Regular.ttf /data/system/theme/fonts/Roboto-Light.ttf
+		ln -s /data/system/theme/fonts/Roboto-Regular.ttf /data/system/theme/fonts/Roboto-LightItalic.ttf
+		ln -s /data/system/theme/fonts/Roboto-Bold.ttf /data/system/theme/fonts/Roboto-Medium.ttf
+		ln -s /data/system/theme/fonts/Roboto-Bold.ttf /data/system/theme/fonts/Roboto-MediumItalic.ttf
+		ln -s /data/system/theme/fonts/Roboto-Regular.ttf /data/system/theme/fonts/Roboto-ThinItalic.ttf
+		ln -s /data/system/theme/fonts/Roboto-Regular.ttf /data/system/theme/fonts/Roboto-ThinItalic.ttf
+		chown -R system_theme /data/system/theme/fonts
+	fi
+fi
+
 #Function to remove original ja
 remove_ja() {
   sed -i -e '/<family lang="ja"/,/<\/family>/d' $1
